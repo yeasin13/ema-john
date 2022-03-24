fa0bf87f6259b6bc7a77af3ab820b0a5836e1d2e
yeasin13 <yeasinarafat8001@gmail.com>
2022-03-24 22:21:56 +0600

    first-add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import logo from '../../images/Logo.svg'
+import logo from '../../images/Logo.svg';
+import './Header.css';
 const Header = () => {
     return (
         <nav className='header'>
            <img src={logo} alt="" /> 
+           <div>
+               <a href="/shop">Shop</a>
+               <a href="/orders">Orders</a>
+               <a href="inventory">Inventory</a>
+               <a href="about">About</a>
+           </div>
         </nav>
     );
 };
