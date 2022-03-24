d3517d671b8b20ff656a16e8cae841c9650c5ba1
yeasin13 <yeasinarafat8001@gmail.com>
2022-03-25 01:49:24 +0600

    cart-add

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProduct] = useState([]);
+const [cart, setCart] = useState([]);
+
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data => setProduct(data))
+    }, []);
+
+    const handleAddToCart = (product) =>{
+        console.log(product);
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
+
+    return (
+        <div className='shop-container'>
+           <div className="products-container">
+               {
+                   products.map(product=><Product key={product.id}
+                    product={product}
+                    handleAddToCart={handleAddToCart}
+                   ></Product>)
+               }
+           </div>
+           <div className="cart-container"><h3>Order Summary</h3>
+           <p>Selected Items: {cart.length}</p>
+           </div>
+        </div>
+    );
+};
+
+export default Shop;
