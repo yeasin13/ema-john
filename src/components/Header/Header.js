797f518f63f9f3587750b838a74606c84fd24c88
yeasin13 <yeasinarafat8001@gmail.com>
2022-03-24 21:43:23 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'>
+           <img src={logo} alt="" /> 
+        </nav>
+    );
+};
+
+export default Header;
